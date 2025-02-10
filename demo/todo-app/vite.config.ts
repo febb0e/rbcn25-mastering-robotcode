dbdbee4675cc3969f734cb567b2a2549b1f0dc47
Fabian Tsirogiannis <fabian.tsirogiannis@imbus.de>
2025-02-10 13:47:40 +0100

    fix: fixed vite app port assignment

--- demo/todo-app/vite.config.ts
+++ demo/todo-app/vite.config.ts
@@ -2,12 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ }) => {
+  const environment = process.env.VITE_ENVIRONMENT || 'Development'
+  const port = environment === 'Test' ? 3000 : 3001
+
   return {
     plugins: [vue()],
     base: '',
     server: {
-      port: mode === 'development' ? 3000 : 3001
+      port: port
     }
   }
 })
